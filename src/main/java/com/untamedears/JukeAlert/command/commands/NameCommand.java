7e5659cb4635380575344d8fe7aabf0078578109
iykHvfIvfR <iykHvfIvfR@users.noreply.github.com>
2019-05-15 00:37:26 +0100

    Make it possible to /janame remote snitches (#48)

--- src/main/java/com/untamedears/JukeAlert/command/commands/NameCommand.java
+++ src/main/java/com/untamedears/JukeAlert/command/commands/NameCommand.java
@@ -1,9 +1,11 @@
 package com.untamedears.JukeAlert.command.commands;
 
-import static com.untamedears.JukeAlert.util.Utility.findLookingAtOrClosestSnitch;
+import com.untamedears.JukeAlert.util.Utility;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -14,6 +16,7 @@ import net.md_5.bungee.api.chat.TextComponent;
 
 import vg.civcraft.mc.civmodcore.command.PlayerCommand;
 import vg.civcraft.mc.namelayer.permission.PermissionType;
+import vg.civcraft.mc.namelayer.NameAPI;
 
 import com.untamedears.JukeAlert.JukeAlert;
 import com.untamedears.JukeAlert.model.Snitch;
@@ -24,8 +27,8 @@ public class NameCommand extends PlayerCommand {
 
 		super("Name");
 		setDescription("Set snitch name");
-		setUsage("/janame <name>");
-		setArguments(1, 1);
+		setUsage("/janame <name> [<x> <y> <z> [<world>]]");
+		setArguments(1, 5);
 		setIdentifier("janame");
 	}
 
@@ -38,9 +41,49 @@ public class NameCommand extends PlayerCommand {
 			return false;
 		}
 		Player player = (Player) sender;
-		Snitch snitch = findLookingAtOrClosestSnitch(player, PermissionType.getPermission("RENAME_SNITCH"));
-		if (snitch == null) {
-			return true;
+		Snitch snitch;
+		if (args.length == 1) {
+			snitch = Utility.findLookingAtOrClosestSnitch(
+				player, PermissionType.getPermission("RENAME_SNITCH"));
+			if (snitch == null) {
+				sender.sendMessage(
+					ChatColor.RED + "You do not own any snitches nearby or lack permission to rename them!");
+				return false;
+			}
+		} else {
+			int x;
+			int y;
+			int z;
+			String world;
+			try {
+				x = Integer.parseInt(args[1]);
+				y = Integer.parseInt(args[2]);
+				z = Integer.parseInt(args[3]);
+				if (args.length == 4) {
+					world = player.getLocation().getWorld().getName();
+				} else {
+					world = args[4];
+				}
+			} catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+				sender.sendMessage(ChatColor.RED + "Invalid coordinates.");
+				return false;
+			}
+			if (Bukkit.getWorld(world) == null) {
+				sender.sendMessage(ChatColor.RED + "Invalid world.");
+				return false;
+			}
+			Location loc = new Location(Bukkit.getWorld(world), x, y, z);
+			snitch = JukeAlert.getInstance().getSnitchManager().getSnitch(loc.getWorld(), loc);
+			if (snitch == null
+					|| !Utility.doesSnitchExist(snitch, true)
+					|| !NameAPI.getGroupManager().hasAccess(
+						snitch.getGroup(),
+						player.getUniqueId(),
+						PermissionType.getPermission("RENAME_SNITCH"))) {
+				sender.sendMessage(
+					ChatColor.RED + "You do not own a snitch at those coordinates or lack permission to rename it!");
+				return false;
+			}
 		}
 
 		String name = "";
